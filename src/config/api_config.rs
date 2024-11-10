718fe6668908862aa211ce60a3d3953917e15025
Harry Seigne <harryseigne@gmail.com>
2024-11-10 11:02:23 +1100

    Add generate_url method to api_config struct

--- src/config/api_config.rs
+++ src/config/api_config.rs
@@ -1,4 +1,11 @@
 use std::env;
+use sha1::Sha1;
+use hmac::{Hmac, KeyInit, Mac};
+use hex;
+
+type HmacSha1 = Hmac<Sha1>;
+const BASE_URL: &str = "http://timetableapi.ptv.vic.gov.au";
+
 pub struct ApiConfig {
     pub dev_id: i32,
     pub key: String,
@@ -15,8 +22,22 @@ impl ApiConfig {
 
         Ok(ApiConfig {
             dev_id,
-            key,
+            key
         })
+    }
+
+    pub fn generate_url(&self, request: &str ) -> Result<String, &'static str> {
+        let separator = if request.contains('?') {'&'} else {'?'};
+
+        let mut mac = HmacSha1::new_from_slice(self.key.as_bytes()).map_err(|_| "Error creating HMAC")?;
+
+        let unsigned_request = format!("{}{}devid={}", request, separator, self.dev_id );
+        mac.update(unsigned_request.as_bytes());
+
+        let signature = mac.finalize().into_bytes();
+        let signature = hex::encode(signature);
+
+        Ok(format!("{BASE_URL}{unsigned_request}&signature={signature}"))
     }
 }
 
@@ -31,6 +52,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn build_with_valid_config() {
         let ptv_dev_id = 12345;
         let ptv_api_key = "my_api_key";
@@ -49,6 +71,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn build_with_missing_dev_id_returns_error() {
         let ptv_api_key = "my_api_key";
 
@@ -66,6 +89,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn build_with_missing_api_key_returns_error() {
         let ptv_dev_id = 12345;
 
@@ -83,6 +107,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn build_with_invalid_dev_id_returns_error() {
         let ptv_dev_id = "this should fail";
         let ptv_api_key = "my_api_key";
@@ -97,6 +122,29 @@ mod tests {
         if let Err(e) = config {
             assert_eq!(e, "Could not parse PTV_DEV_ID to number");
         };
+
+        cleanup_env();
+    }
+
+    #[test]
+    #[ignore]
+    fn generate_url() {
+        let ptv_dev_id = 12345;
+        let ptv_api_key = "my_api_key";
+
+        env::set_var("PTV_DEV_ID", ptv_dev_id.to_string());
+        env::set_var("PTV_API_KEY", ptv_api_key);
+
+        let config = ApiConfig::build();
+
+        let config = config.unwrap();
+
+        assert_eq!(config.dev_id, ptv_dev_id);
+        assert_eq!(config.key, ptv_api_key);
+
+        let url = config.generate_url("my_url");
+
+        assert!(url.is_ok());
 
         cleanup_env();
     }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 mod config;
 
+use std::error::Error;
 use config::api_config::ApiConfig;
 
-fn main() {
-    match ApiConfig::build() {
-        Ok(config) => {
-            println!("Dev ID: {}", config.dev_id);
-            println!("API Key: {}", config.key);
-        }
-        Err(e) => eprintln!("Failed to build ApiConfig: {}", e),
-    }
+fn main() -> Result<(), Box<dyn Error>>  {
+    let config = ApiConfig::build()?;
+
+    println!("Config built!");
+    println!("Generating URL for /v3/departures/route_type/1/stop/2043/route/1881");
+
+    let url = config.generate_url("/v3/departures/route_type/1/stop/2043/route/1881")?;
+
+    println!("Generated URL was:");
+    println!("{}", url);
+
+    Ok(())
 }
